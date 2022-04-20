1b2f765d5a9b3d72185c520b3516c3b0c43ceb7e
Coovik <loopyrm@gmail.com>
2022-04-21 01:02:56 +0300

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Dialogs from "./components/dialogs/Dialogs";
 import Header from "./components/header/Header";
 import Profile from "./components/profile/Profile";
 import Sidebar from "./components/sidebar/Sidebar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
    return (
-      <div className='app-wrapper'>
-         <Header />
-         <Sidebar />
-         <div className="app-content">
-            <Profile />
-            {/* <Dialogs /> */}
-         </div>
-      </div >
+      <BrowserRouter>
+         <div className='app-wrapper'>
+            <Header />
+            <Sidebar />
+            <div className="app-content">
+               <Routes>
+                  <Route path="/Profile" element={<Profile />} />
+                  <Route path="/Dialogs" element={<Dialogs />} />
+               </Routes>
+            </div>
+         </div >
+      </BrowserRouter>
    );
 }
 
