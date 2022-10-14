fedc5495068aa16e0469c4b8f1ac8a6fd6442a1c
Coovik <loopyrm@gmail.com>
2022-10-14 12:10:19 +0300

    first step to formik

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,35 +1,55 @@
+import { Field, ErrorMessage, Form, Formik } from 'formik'
 import { connect } from 'react-redux'
-import { Field, reduxForm } from 'redux-form'
-import { required } from '../../utils/validators/validators'
-import { Input } from '../common/formsControl/FormsContol'
-import { login } from "./../../redux/auth-reducer"
 import { Navigate } from "react-router-dom"
-import c from '../common/formsControl/FormsControl.module.css'
+import { login } from "./../../redux/auth-reducer"
 
-
-const LoginForm = props => {
-   return <form onSubmit={props.handleSubmit}>
-      <div>
-         <Field name='email' placeholder='email'
-            component={Input} type='text'
-            validate={[required]} />
-      </div>
-      <div>
-         <Field name='password' placeholder='password'
-            component={Input} type='password'
-            validate={[required]} />
-      </div>
-      <div>
-         <Field name='rememberMe' component='input' type='checkbox' /> Remember me
-      </div>
-      {props.error && <div className={c.formSummaryError}>
-         {props.error}
-      </div>}
-      <button>Login</button>
-   </form>
+const LoginForm2 = props => {
+   return <Formik
+      initialValues={{ email: '', password: '', rememberMe: false }}
+      onSubmit={props.onSubmit}
+   >
+      <Form>
+         <div>
+            <label htmlFor='email'>Email</label><br />
+            <Field id='email' name='email' type='email' /><br />
+            <ErrorMessage name="email" />
+         </div>
+         <div>
+            <label htmlFor='password'>Password</label><br />
+            <Field id='password' name='password' type='password' /><br />
+            <ErrorMessage name="password" />
+         </div>
+         <div>
+            <Field type='checkbox' name='rememberMe' />Remember me<br />
+            <ErrorMessage name="rememberMe" />
+         </div>
+         <button>Submit</button>
+      </Form>
+   </Formik>
 }
+// const LoginForm = props => {
+//    return <form onSubmit={props.handleSubmit}>
+//       <div>
+//          <Field name='email' placeholder='email'
+//             component={Input} type='text'
+//             validate={[required]} />
+//       </div>
+//       <div>
+//          <Field name='password' placeholder='password'
+//             component={Input} type='password'
+//             validate={[required]} />
+//       </div>
+//       <div>
+//          <Field name='rememberMe' component='input' type='checkbox' /> Remember me
+//       </div>
+//       {props.error && <div className={c.formSummaryError}>
+//          {props.error}
+//       </div>}
+//       <button>Login</button>
+//    </form>
+// }
 
-const LoginRedaxForm = reduxForm({ form: 'login' })(LoginForm)
+// const LoginRedaxForm = reduxForm({ form: 'login' })(LoginForm)
 
 const Login = props => {
    const onSubmit = formData => props.login(formData.email, formData.password, formData.rememberMe)
@@ -38,7 +58,7 @@ const Login = props => {
       <h1>Enter this data</h1>
       <div>Email: free@samuraijs.com</div>
       <div>Password: free</div>
-      <LoginRedaxForm onSubmit={onSubmit} />
+      <LoginForm2 onSubmit={onSubmit} />
    </>
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import store from './redux/redux-store';
-import App from './App';
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import store from './redux/redux-store'
+import App from './App'
+import { Provider } from 'react-redux'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
